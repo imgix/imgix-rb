bf1e5966780dd111e244c168146dc15e1b2b25e4
Kelly Sutton <kelly@imgix.com>
2015-05-13 19:50:28 -0700

    Fixes an issue with encodings when full URLs are present as the imgix path component for a Web Proxy source.

--- test/units/domains_test.rb
+++ test/units/domains_test.rb
@@ -37,7 +37,6 @@ class DomainsTest < Imgix::Test
 
     path = client.path('/bridge.png')
     assert_equal 'http://demos-1.imgix.net/bridge.png?&s=13e68f249172e5f790344e85e7cdb14b', path.to_url
-
   end
 
   def test_strips_out_protocol
@@ -47,7 +46,18 @@ class DomainsTest < Imgix::Test
 
     path = client.path('/bridge.png')
     assert_equal 'http://demos-1.imgix.net/bridge.png?&s=13e68f249172e5f790344e85e7cdb14b', path.to_url
-
   end
 
+  def test_with_full_paths
+    client = Imgix::Client.new(:hosts => [
+        "demos-1.imgix.net",
+        "demos-2.imgix.net",
+        "demos-3.imgix.net",
+      ],
+      :token => '10adc394',
+      :shard_strategy => :cycle)
+
+    path = 'https://google.com/cats.gif'
+    assert_equal "http://demos-1.imgix.net/#{CGI.escape(path)}?&s=4c3ff935011f0d2251800e6a2bb68ee5", client.path(path).to_url
+  end
 end
