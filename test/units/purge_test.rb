d49eab3465de6f6a25c07db5e7a4a44d2931f062
Eric Sanchez <esanchez@imgix.com>
2020-08-12 16:31:41 -0700

    refactor(style): enforce double quoted strings

--- test/units/purge_test.rb
+++ test/units/purge_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 class PurgeTest < Imgix::Test
   def test_runtime_error_without_api_key
@@ -13,19 +13,19 @@ class PurgeTest < Imgix::Test
     stub_request(:post, endpoint).with(body: body).to_return(status: 200)
 
     assert_output(nil, deprecation_warning) do
-      mock_client(api_key: '10adc394').purge('/images/demo.png')
+      mock_client(api_key: "10adc394").purge("/images/demo.png")
     end
   end
 
   def test_successful_purge
     stub_request(:post, endpoint).with(body: body).to_return(status: 200)
 
-    mock_client(api_key: '10adc394').purge('/images/demo.png')
+    mock_client(api_key: "10adc394").purge("/images/demo.png")
 
     assert_requested(
       :post,
       endpoint,
-      body: 'url=https%3A%2F%2Fdemo.imgix.net%2Fimages%2Fdemo.png',
+      body: "url=https%3A%2F%2Fdemo.imgix.net%2Fimages%2Fdemo.png",
       headers: mock_headers,
       times: 1
     )
@@ -33,9 +33,9 @@ class PurgeTest < Imgix::Test
 
   private
 
-  def mock_client(api_key: '')
+  def mock_client(api_key: "")
     Imgix::Client.new(
-      domain: 'demo.imgix.net',
+      domain: "demo.imgix.net",
       api_key: api_key,
       include_library_param: false
     )
@@ -43,24 +43,24 @@ class PurgeTest < Imgix::Test
 
   def mock_headers
     {
-      'Accept' => '*/*',
-      'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      'Authorization' => 'Basic MTBhZGMzOTQ6',
-      'Content-Type' => 'application/x-www-form-urlencoded',
-      'User-Agent' => "imgix rb-#{Imgix::VERSION}"
+      "Accept" => "*/*",
+      "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+      "Authorization" => "Basic MTBhZGMzOTQ6",
+      "Content-Type" => "application/x-www-form-urlencoded",
+      "User-Agent" => "imgix rb-#{Imgix::VERSION}"
     }
   end
 
   def mock_image
-    'https://demo.imgix.net/images/demo.png'
+    "https://demo.imgix.net/images/demo.png"
   end
 
   def endpoint
-    'https://api.imgix.com/v2/image/purger'
+    "https://api.imgix.com/v2/image/purger"
   end
 
   def body
-    { 'url' => mock_image }
+    { "url" => mock_image }
   end
 
   def deprecation_warning
--- test/units/url_test.rb
+++ test/units/url_test.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 class UrlTest < Imgix::Test
-  DEMO_IMAGE_PATH = '/images/demo.png'
+  DEMO_IMAGE_PATH = "/images/demo.png"
 
   def test_signing_with_no_params
     path = client.path(DEMO_IMAGE_PATH)
 
-    assert_equal 'https://demo.imgix.net/images/demo.png?s=2c7c157eaf23b06a0deb2f60b81938c4', path.to_url
+    assert_equal "https://demo.imgix.net/images/demo.png?s=2c7c157eaf23b06a0deb2f60b81938c4", path.to_url
   end
 
   def test_signing_with_one
@@ -16,11 +16,11 @@ class UrlTest < Imgix::Test
 
     assert_output(nil, "Warning: `Path.width=' has been deprecated and " \
       "will be removed in the next major version (along " \
-      "with all parameter `ALIASES`).\n") {
-        path.width = 200
-    }
+      "with all parameter `ALIASES`).\n") do
+      path.width = 200
+    end
 
-    assert_equal 'https://demo.imgix.net/images/demo.png?w=200&s=da421114ca238d1f4a927b889f67c34e', path.to_url
+    assert_equal "https://demo.imgix.net/images/demo.png?w=200&s=da421114ca238d1f4a927b889f67c34e", path.to_url
   end
 
   def test_signing_with_multiple_params
@@ -28,53 +28,56 @@ class UrlTest < Imgix::Test
 
     assert_output(nil, "Warning: `Path.height=' has been deprecated and " \
       "will be removed in the next major version (along " \
-      "with all parameter `ALIASES`).\n") {
-        path.height = 200
-    }
+      "with all parameter `ALIASES`).\n") do
+      path.height = 200
+    end
 
     assert_output(nil, "Warning: `Path.width=' has been deprecated and " \
       "will be removed in the next major version (along " \
-      "with all parameter `ALIASES`).\n") {
-        path.width = 200
-    }
+      "with all parameter `ALIASES`).\n") do
+      path.width = 200
+    end
 
-    assert_equal 'https://demo.imgix.net/images/demo.png?h=200&w=200&s=d570a1ecd765470f7b34a69b56718a7a', path.to_url
+    assert_equal "https://demo.imgix.net/images/demo.png?h=200&w=200&s=d570a1ecd765470f7b34a69b56718a7a", path.to_url
 
     path = client.path(DEMO_IMAGE_PATH)
 
     assert_output(nil, "Warning: `Path.width=' has been deprecated and " \
       "will be removed in the next major version (along " \
-      "with all parameter `ALIASES`).\n") {
-        path.width = 200
-    }
+      "with all parameter `ALIASES`).\n") do
+      path.width = 200
+    end
 
     assert_output(nil, "Warning: `Path.height=' has been deprecated and " \
       "will be removed in the next major version (along " \
-      "with all parameter `ALIASES`).\n") {
-        path.height = 200
-    }
+      "with all parameter `ALIASES`).\n") do
+      path.height = 200
+    end
 
-    assert_equal 'https://demo.imgix.net/images/demo.png?w=200&h=200&s=00b5cde5c7b8bca8618cb911da4ac379', path.to_url
+    assert_equal "https://demo.imgix.net/images/demo.png?w=200&h=200&s=00b5cde5c7b8bca8618cb911da4ac379", path.to_url
   end
 
   def test_domain_resolves_host_warn
     assert_output(nil, "Warning: The identifier `host' has been deprecated and " \
       "will\nappear as `domain' in the next major version, e.g. " \
       "`@host'\nbecomes `@domain', `options[:host]' becomes " \
-      "`options[:domain]'.\n") {
-        Imgix::Client.new(host: 'demo.imgix.net', include_library_param: false)
-    }
-
+      "`options[:domain]'.\n") do
+      Imgix::Client.new(host: "demo.imgix.net", include_library_param: false)
+    end
 
     # Assert the output of this call (to both stdout and stderr) is nil.
-    assert_output(nil, nil) {
-      Imgix::Client.new(domain: 'demo.imgix.net', include_library_param: false)
-    }
+    assert_output(nil, nil) do
+      Imgix::Client.new(domain: "demo.imgix.net", include_library_param: false)
+    end
   end
 
-private
+  private
 
   def client
-    @client ||= Imgix::Client.new(host: 'demo.imgix.net', secure_url_token: '10adc394', include_library_param: false)
+    @client ||= Imgix::Client.new(
+      host: "demo.imgix.net",
+      secure_url_token: "10adc394",
+      include_library_param: false
+    )
   end
 end
