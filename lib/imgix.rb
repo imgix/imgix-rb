45dd908ddc0a9eebd8c10f492b27c8e33742ecee
Pedro Nascimento <pnascimento@gmail.com>
2020-11-04 15:02:45 -0300

    feat: Allow domain to have a port
    This allows us to use imgix lib without much changes on local
    environment. We can then have the source be something like
    "localhost.me:3000" and it just works, pointing to the right asset but
    without any transformations.
    
    Without this, devs who can't use S3 on the local environment have to
    resort to hacks on their code to workaround this small regex test, which
    is not ideal.

--- lib/imgix.rb
+++ lib/imgix.rb
@@ -6,7 +6,7 @@ require "imgix/path"
 
 module Imgix
   # regex pattern used to determine if a domain is valid
-  DOMAIN_REGEX = /^(?:[a-z\d\-_]{1,62}\.){0,125}(?:[a-z\d](?:\-(?=\-*[a-z\d])|[a-z]|\d){0,62}\.)[a-z\d]{1,63}$/i.freeze
+  DOMAIN_REGEX = /^(?:[a-z\d\-_]{1,62}\.){0,125}(?:[a-z\d](?:\-(?=\-*[a-z\d])|[a-z]|\d){0,62}\.)[a-z\d]{1,63}(:\d+)?$/i.freeze
 
   # determines the growth rate when building out srcset pair widths
   DEFAULT_WIDTH_TOLERANCE = 0.08
--- test/units/domains_test.rb
+++ test/units/domains_test.rb
@@ -3,6 +3,10 @@
 require 'test_helper'
 
 class DomainsTest < Imgix::Test
+  def test_valid_port_on_domain
+    Imgix::Client.new(domain: "localhost.me:3000")
+  end
+
   def test_invalid_domain_append_slash
     assert_raises(ArgumentError) {Imgix::Client.new(domain: "assets.imgix.net/")}
   end
