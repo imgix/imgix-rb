d0dd15e44e032b93d866dfed57a01e2f099b0139
Luis Ball <lball@imgix.com>
2021-04-05 12:42:24 -0400

    test(minitest): use reporters gem to create human readbale output

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,5 +8,10 @@ require "minitest/autorun"
 require "imgix"
 require "webmock/minitest"
 
+require 'minitest/autorun'
+require 'minitest/reporters' # requires the gem
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
 class Imgix::Test < MiniTest::Test
 end
